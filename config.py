7249224c4b1c67f07cd5fbb815adf367eac5d458
arjunsehajpal <arjunsehajpal2@gmail.com>
2023-05-05 00:25:16 +0530

    add config.py

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+
+
+class Configurations:
+    RESULT_DATA_PATH = os.path.join("data", "internationals", "results.csv")
+    RANKINGS_DATA_PATH = os.path.join("data", "internationals", "rankings-2022-10-06.csv")
+    QUALFIED_TEAMS = [
+        "Australia",
+        "Iran",
+        "Japan",
+        "Qatar",
+        "Saudi Arabia",
+        "South Korea",
+        "Cameroon",
+        "Ghana",
+        "Morocco",
+        "Senegal",
+        "Tunisia",
+        "Canada",
+        "Costa Rica",
+        "Mexico",
+        "United States",
+        "Argentina",
+        "Brazil",
+        "Ecuador",
+        "Uruguay",
+        "Belgium",
+        "Croatia",
+        "Denmark",
+        "England",
+        "France",
+        "Germany",
+        "Netherlands",
+        "Poland",
+        "Portugal",
+        "Serbia",
+        "Spain",
+        "Switzerland",
+        "Wales",
+    ]
+    START_DATE = "2018-08-01"
+    END_DATE = "2022-11-20"
